8ed279672f2e83128e1b69f7845a72cdaeb12b71
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-01-28 14:40:06 -0800

    Create ericca_inventory.h
    My version of the inventory class which is associated with the Ship and Crew classes then depends only on Item class.

---
+++ ericca_inventory.h
@@ -0,0 +1,45 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#define MAX_SPACE 100;
+
+#include <iostream>
+using namespace std;
+
+class Inventory {
+  	private:
+		// want to be able to display so user can see how much space is left
+		int filledSpace, emptySpace, foodSpace, ammoSpace, repairSpace;
+
+		// using Item class to help create an array
+		Item inventorySpace[MAX_SPACE];
+	
+	public:
+		// sets filledSpace to 0
+    	Inventory();
+		~Inventory();
+
+		// returns an integer value to change the space values
+		// takes an Item array
+		int addItem(Item inventorySpace[]);
+		int getItem(Item inventorySpace[]);
+		int useItem(Item inventorySpace[]);
+		
+		int getFilledSpace();
+		void setFilledSpace(int space);
+		
+		int getEmptySpace();
+		void setEmptySpace(int space);
+		
+		int getFoodSpace();
+		void setFoodSpace(int space);
+		
+		int getAmmoSpace();
+		void setAmmoSpace(int space);
+		
+		int getRepairSpace();
+		void setRepairSpace(int space);
+		
+		bool check();
+};
+
+#endif 
