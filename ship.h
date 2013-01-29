296a5e223ea3fb3dc38e28c8c0a716caeae79f0a
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-01-28 16:05:57 -0800

    Update ship.h

--- ship.h
+++ ship.h
@@ -1,12 +1,27 @@
 #ifndef SHIP_H
 #define SHIP_H
 
-class Ship {
-    // should  we create const numbers for maxHealth, maxSpeed, and maxAttack?
-    #define MAX_HEALTH 100;
-    #define MAX_SPEED 10;
-    #define MAX_ATTACK 20;
- 
+#define MAX_HEALTH 100
+#define MAX_SPEED 10
+#define MAX_ATTACK 20
+
+class Ship { 
+	private:
+    	// Exclusive only to this class
+        int shipExplosiveCannonball();
+
+      	// Same things?
+        // I'm not sure what to write for these
+		// parameter for ship to raid/loot on
+		// should this be in inventory instead? since it adds to inventory.
+        void raidShip();
+		void lootShip();
+	
+		Inventory shipInventory;
+		Crew shipCrew;
+        string shipName;
+        int attack, health, speed;
+
     // Other ships can have these public functions
     public:
         Ship(); // initializes some private values;
@@ -32,22 +47,6 @@ class Ship {
         
         void shipUseItem(Item[]); // uses weapons and repair items
         void crewUseItem(Item[]); // uses food and booze
-
-    private:
-        // Exclusive only to this class
-        int shipExplosiveCannonball();
-
-      	// Same things?
-        // I'm not sure what to write for these
-	// parameter for ship to raid/loot on
-	// should this be in inventory instead? since it adds to inventory.
-        void raidShip();
-	void lootShip();
-	
-	Inventory shipInventory;
-	Crew shipCrew;
-        string shipName;
-        int attack, health, speed;
 };
 
 #endif
