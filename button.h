c05875820c1081c443b047616578593effa03035
SamClose <soclosesam@gmail.com>
2013-01-30 01:55:04 -0800

    Update button.h

--- button.h
+++ button.h
@@ -1,28 +1,23 @@
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
+#include <button.cpp>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include<allegro5/allegro_ttf.h>
+#include<allegro5/allegro_font.h>
+#include <iostream>
+
+
+
 //parent class should represent a button in a menu that will change color when selected(up/down)
-//When "Enter" is pressed, should perform a certain action, determined by child class
+//When "Enter" is pressed, should perform a certain action, determined by instance
 
-class Button {
-
-#define DEFAULT_COLOR black; //come back to this
-#define SELECTED_COLOR red;
-
+class Button{
 
 public:
-  string getColor();
-	void setColor()
-	bool getSelected;
-	void setSelected();
-
-
-
-private:
-	string color; //might be better to do color as an int 0-255 corresponding to a color value(r,g,b)
-	bool selected;
-
-
+	Button();
+	void printText(int fontSize,const char *filename, int red, int green, int blue, float x, float y, int flags, const char *text);
 };
 
 #endif /* BUTTON_H_ */
