f2a48b8d67cef363aa4c9fdaa15519db18257d83
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-04 14:49:00 -0800

    Create crew.cpp
    Implementation for crew.h. Associates(?) itself with Inventory class by taking an Inventory object as a parameter.

---
+++ crew.cpp
@@ -0,0 +1,44 @@
+// Add other invludes if need be
+#include <iostream>
+#include "crew.h"
+
+using namespace std;
+
+Crew::Crew() {
+	mutinyLevel = MIN_MUTINY_LEVEL;
+	happinessLevel = MAX_HAPPY_LEVEL;
+	crewNumber = MAX_CREW_NUMBER;
+}
+
+Crew::~Crew() {
+}
+
+void Crew::crewUseItem(Inventory inventory) {
+}
+
+int Crew::getCrewNumber() {
+	return crewNumber;
+}
+
+void Crew::setCrewNumber(int number) {
+	crewNumber = number;
+}
+
+int Crew::getMutinyLevel() {
+	return mutinyLevel;
+}
+
+void Crew::setMutinylevel(int level) {
+	mutinyLevel = level;
+}
+
+int Crew::getHappinessLevel() {
+	return happinessLevel;
+}
+
+void Crew::setHappinessLevel(int level) {
+	happinessLevel = level;
+}
+
+/* End of Crew.cpp */
+	
