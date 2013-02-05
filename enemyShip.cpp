bb8caba5904f676b41fcde2fa1d578a56b3c4b65
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-04 16:18:45 -0800

    Create enemyShip.cpp
    Implementation of enemyShip.h

---
+++ enemyShip.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "enemyShip.h"
+
+using namespace std;
+
+EnemyShip::EnemyShip() {
+	health = MAX_ENEMY_HEALTH;
+	attack = MAX_ENEMY_ATTACK;
+	speed = MAX_ENEMY_SPEED;
+}
+
+virtual EnemyShip::~EnemyShip() {
+	inventorySpace.~Inventory(); // virtual call
+}
+
+int EnemyShip::shipCannonBall() {
+	return attack;
+}
+
+int EnemyShip::shipScatterShot() {
+	return attack/4;
+}
+
+int EnemyShip::shipChainShot() {
+	return attack/speed;
+}
+
+/* End of enemyShip.cpp */
