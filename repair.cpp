f25ea42c1485a457e76cbf8d650495c36270801b
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-06 14:18:22 -0800

    Create repair.cpp
    Implementation of repair.h

---
+++ repair.cpp
@@ -0,0 +1,12 @@
+#include "repair.h"
+
+using namespace std;
+
+int Repair::getHealthBonus() {
+	return healthBonus;
+}
+
+void Repair::setHealthBonus(int bonus) {
+	healthBonus = bonus;
+}
+/* End of repair.cpp */
