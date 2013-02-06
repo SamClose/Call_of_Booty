e6421085241c4703a24cdf14aab5a54017f98c84
SamClose <soclosesam@gmail.com>
2013-02-06 12:47:49 -0800

    Create introScreen.h

---
+++ introScreen.h
@@ -0,0 +1,13 @@
+#ifndef INTRO_SCREEN
+#define INTRO_SCREEN
+
+#include "button.h"
+
+class IntroScreen{
+
+public:
+  IntroScreen();
+	void makeIntroScreen(float ScreenWidth);
+
+};
+#endif //INTRO_SCREEN
