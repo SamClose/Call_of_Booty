91bd9bb6b23e83c68a184aea9759403db40261b7
SamClose <soclosesam@gmail.com>
2013-02-06 12:45:39 -0800

    Update button.h

--- button.h
+++ button.h
@@ -1,7 +1,7 @@
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
-#include <button.cpp>
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 #include<allegro5/allegro_ttf.h>
@@ -11,13 +11,13 @@
 
 
 //parent class should represent a button in a menu that will change color when selected(up/down)
-//When "Enter" is pressed, should perform a certain action, determined by instance
+//When clicked on by a mouse, should perform a certain action, determined by instance--not yet implemented
 
 class Button{
 
 public:
 	Button();
-	void printText(int fontSize,const char *filename, int red, int green, int blue, float x, float y, int flags, const char *text);
+	 void printText(int fontSize,const char *filename, int red, int green, int blue, float x, float y, int flags, const char *text);
 };
 
-#endif /* BUTTON_H_ */
+#endif //BUTTON_H_
