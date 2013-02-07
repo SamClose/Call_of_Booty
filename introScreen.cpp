602ebbabb9bc857ed45e17926c22a6f9c2b51c36
SamClose <soclosesam@gmail.com>
2013-02-07 11:17:01 -0800

    Update introScreen.cpp

--- introScreen.cpp
+++ introScreen.cpp
@@ -4,6 +4,7 @@
 #include <allegro5/allegro_native_dialog.h>
 #include<allegro5/allegro_ttf.h>
 #include<allegro5/allegro_font.h>
+#include<allegro5/allegro_image.h>
 #include "introScreen.h"
 
 using namespace std;
@@ -12,20 +13,27 @@ IntroScreen::IntroScreen(){
 }
 
 void IntroScreen::makeIntroScreen(float ScreenWidth){
-  //draw "Call Of Booty" at top using a jpg or similar existing image
+	//draw "Call Of Booty" at top using a jpg or similar existing image
 	
 	//create 3 buttons for introscreen
 	//al_init_font_addon();
-	//al_init_ttf_addon();
+	al_init_ttf_addon();
+	al_init_image_addon();
+
+	ALLEGRO_BITMAP *skull = al_load_bitmap("skull.png");
+	al_draw_bitmap(skull,0,0,NULL);
+
+	ALLEGRO_BITMAP *title = al_load_bitmap("Title.png");
+	al_draw_bitmap( title,0,0,NULL); 
 
 	Button playButton;
-	playButton.printText(36, "Pirate.ttf",255,255,255,ScreenWidth/2,200, ALLEGRO_ALIGN_CENTRE,"Play Game");
+	playButton.printText(85, "Pirate.ttf",255,0,0,ScreenWidth/2,150, ALLEGRO_ALIGN_CENTRE,"Play Game");
 
 	Button howButton;
-	howButton.printText(36, "Pirate.ttf",255,255,255,ScreenWidth/2,300, ALLEGRO_ALIGN_CENTRE,"How to Play");
+	howButton.printText(85, "Pirate.ttf",255,0,0,ScreenWidth/2,300, ALLEGRO_ALIGN_CENTRE,"How to Play");
 
 	Button exitButton;
-	exitButton.printText(36, "Pirate.ttf",255,255,255,ScreenWidth/2,400, ALLEGRO_ALIGN_CENTRE,"Exit Game");
+	exitButton.printText(85, "Pirate.ttf",255,0,0,ScreenWidth/2,450, ALLEGRO_ALIGN_CENTRE,"Exit Game");
 
 
 
