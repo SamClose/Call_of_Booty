815bc8df276fad5f61ffc97e59cc944ad53caa3d
SamClose <soclosesam@gmail.com>
2013-02-12 15:38:12 -0800

    Create howToPlay.h

---
+++ howToPlay.h
@@ -0,0 +1,17 @@
+#ifndef HOW_TO_PLAY
+#define HOW_TO_PLAY
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include<allegro5/allegro_ttf.h>
+#include<allegro5/allegro_font.h>
+#include <iostream>
+
+class HowToPlay{
+
+public:
+  HowToPlay();
+	void makeHowToPlayScreen();
+
+};
+#endif //HOW_TO_PLAY
