eb37574ba44a997be4f2444bff08701eb360a686
SamClose <soclosesam@gmail.com>
2013-02-12 15:36:50 -0800

    Update introScreen.h

--- introScreen.h
+++ introScreen.h
@@ -2,12 +2,13 @@
 #define INTRO_SCREEN
 
 #include "button.h"
+#include "howToPlay.h"
 
 class IntroScreen{
 
 public:
-  IntroScreen();
-	void makeIntroScreen(float ScreenWidth);
+	IntroScreen();
+	void makeIntroScreen(float ScreenWidth, float ScreenHeight);
 
 };
 #endif //INTRO_SCREEN
