04af193e4c53573a73d5a329658eea96a9cd5449
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-13 14:34:40 -0800

    Update shipBattle.h

--- shipBattle.h
+++ shipBattle.h
@@ -8,24 +8,21 @@ class ShipBattle {
 		Ship enemy;
 
 	public:
-        // Creates a ship battle
-        ShipBattle(Ship mainShip, EnemyShip enemyShip);
-        
-        // This is a method to allow mainShip to attack first and EnemyShip randomly decides what to do next.
-        void shipBattle();
-        
-        // Draws both HP bars, names for both, attack menu, inventory menu, and flee.
-       	void drawShipBattleFrame();
-       	
-        // Updates after each turn during battle.
-       	void update();
-        
-        // Getters and Setters
-        Ship getMainShip();
-        void setMainShip(Ship newShip);
-        
-        EnemyShip getEnemyShip();
-        void setEnemy(EnemyShip newEnemy);
+	        // Creates a ship battle
+	        ShipBattle(Ship mainShip, EnemyShip enemyShip);
+	        
+	        // This is a method to allow mainShip to attack first and EnemyShip randomly decides what to do next.
+	        void shipBattle();
+	        
+	        // Draws both HP bars, names for both, attack menu, inventory menu, and flee.
+	       	void update();
+	        
+	        // Getters and Setters
+	        Ship getMainShip();
+	        void setMainShip(Ship newShip);
+	        
+	        EnemyShip getEnemyShip();
+	        void setEnemy(EnemyShip newEnemy);
 
 }
 #endif
