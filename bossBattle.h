54d02f909deda716384898e9e72c83d4c0b9d6cb
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-13 09:58:15 -0800

    Create bossBattle.h
    Simulation of a Boss battle in Call of Booty with Nessy the Loch Ness Monster.

---
+++ bossBattle.h
@@ -0,0 +1,12 @@
+#ifndef BOSS_BATTLE_H
+#define BOSS_BATTLE_H
+
+class BossBattle {
+
+  public:
+    // Same as shipBattle but with the boss.
+    // Should add more attacks to Boss class to allow an interesting battle other than just sit and attack.
+    bossBattle(Ship mainShip, Boss Nessy);
+
+}
+#endif
