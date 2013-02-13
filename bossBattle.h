77d7301a004432fec72814c63e433b1caf1c9082
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-13 14:25:59 -0800

    Update bossBattle.h

--- bossBattle.h
+++ bossBattle.h
@@ -2,15 +2,29 @@
 #define BOSS_BATTLE_H
 
 class BossBattle {
+    private:
+        Ship main;
+        Boss boss;
 
     public:
-        // Same as shipBattle but with the boss.
+        // Creates a boss battle frame.
+        BossBattle(Ship mainShip, Boss Nessy);
+        
         // Should add more attacks to Boss class to allow an interesting battle other than just sit and attack.
-        bossBattle(Ship mainShip, Boss Nessy);
+        void bossBattle();
+        
         // Draws both HP bars, both names, attack menu, inventory, and flee.
         void drawBossBattleFrame();
+        
         // Updates window after each turn.
         void update();
+        
+        // Just added these for semantics.
+        Ship getMainShip();
+        void setMainShip(Ship newShip);
+
+        Boss getBoss();
+        void setBoss(Boss newBoss);
 
 }
 #endif
