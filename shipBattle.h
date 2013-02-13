d4e829a9b6ca4d48ee1aafe1e2e77b904675d7ee
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-13 14:07:17 -0800

    Update shipBattle.h

--- shipBattle.h
+++ shipBattle.h
@@ -3,9 +3,13 @@
 
 class ShipBattle {
 
-  public:
-    // This is a method to allow mainShip to attack first and EnemyShip randomly decides what to do next.
-    ShipBattle(Ship mainShip, EnemyShip ship);
+    public:
+        // This is a method to allow mainShip to attack first and EnemyShip randomly decides what to do next.
+        ShipBattle(Ship mainShip, EnemyShip ship);
+        // Draws both HP bars, names for both, attack menu, inventory menu, and flee.
+       	void drawShipBattleFrame();
+       	// Updates after each turn during battle.
+       	void update();
 
 }
 #endif
