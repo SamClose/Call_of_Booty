c2bf9fa6698d60bd07d85ec0122da28cb79a33ca
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-20 14:24:15 -0800

    Update bossBattle.h

--- bossBattle.h
+++ bossBattle.h
@@ -1,10 +1,12 @@
 #ifndef BOSS_BATTLE_H
 #define BOSS_BATTLE_H
+#include <allegro5/allegro.h>
 
 class BossBattle {
     private:
         Ship main;
         Boss boss;
+        ALLEGRO_DISPLAY *display;
 
     public:
         // Creates a boss battle frame.
@@ -14,6 +16,7 @@ class BossBattle {
         void bossBattle();
         
         // Draws both HP bars, both names, attack menu, inventory, and flee.
+        void drawBossBattle(ALLEGRO_DISPLAY *display);
         void update();
         
         // Just added these for semantics.
@@ -22,6 +25,9 @@ class BossBattle {
 
         Boss getBoss();
         void setBoss(Boss newBoss);
+
+        ALLEGRO_DISPLAY& getDisplay();
+        void setDisplay(ALLEGRO_DISPLAY *display);
 
 }
 #endif
