41b121224c3d96cb91e30ddeed952217d3d2b966
SamClose <soclosesam@gmail.com>
2013-02-21 02:23:47 -0800

    Update introScreen.h

--- introScreen.h
+++ introScreen.h
@@ -9,6 +9,23 @@ class IntroScreen{
 public:
 	IntroScreen();
 	void makeIntroScreen(float ScreenWidth, float ScreenHeight);
+	bool getDone();
+	void setDone(bool doneValue);
+	bool getExists();
+	void setExists(bool existsValue);
+	bool getPlayGo();
+	void setPlayGo(bool playGoValue);
+	bool getHowGo();
+	void setHowGo(bool howGoValue);
+	bool getExit();
+	void setExit(bool exitValue);
 
+private:
+	bool done;
+	bool exists;
+	bool playGo;
+	bool howGo;
+	bool exit;
+	
 };
 #endif //INTRO_SCREEN
