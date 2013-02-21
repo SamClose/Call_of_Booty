e1f906f970490a95963a7ff6a5250b61a43d34d8
SamClose <soclosesam@gmail.com>
2013-02-21 02:23:18 -0800

    Update howToPlay.cpp

--- howToPlay.cpp
+++ howToPlay.cpp
@@ -11,14 +11,15 @@
 using namespace std;
 
 HowToPlay::HowToPlay(){
+	howDone = false;
 }
 
 void HowToPlay::makeHowToPlayScreen(){
-  //when activated, show the "how to play" screen
+	//when activated, show the "how to play" screen
 	//when a button/key is clicked/pressed : erase this to show the introscreen again
 	//the click/button settings from introscreen might still be active, so might have to handle this differently
 	//i.e. by killing introscreen, loading "how to play" screen, then killing it and reloading introscreen upon action.
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queueH = NULL;
 
 	//al_init_font_addon();
 	//al_init_ttf_addon();
@@ -27,10 +28,58 @@ void HowToPlay::makeHowToPlayScreen(){
 	al_install_keyboard();
 	al_init_primitives_addon();
 
+	event_queueH = al_create_event_queue();
+	al_register_event_source(event_queueH,al_get_mouse_event_source());
+
+	al_draw_filled_rectangle(0,0,800,600,al_map_rgb(0,0,0));
 
 	ALLEGRO_FONT *font = al_load_font("Pirate.ttf", 45, NULL);
-	al_draw_text(font, al_map_rgb(255,0,0),0,0,NULL,"Temp filler text");
+	al_draw_text(font, al_map_rgb(255,0,0),0,0,NULL,"Click to go back, here's some more filler text");
 
 	al_flip_display();
+
+	setHowDone(false);
+
+	while(!getHowDone())
+	{
+		ALLEGRO_EVENT evH;
+		al_wait_for_event(event_queueH, &evH);
+		if(evH.type ==ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			setHowDone(true);
+		}
+		else if(evH.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+		{
+			if (evH.mouse.button & 1)
+			{
+
+				al_draw_filled_rectangle(400, 300,415,315,al_map_rgb(255,255,0));
+				setHowDone(true);
+
+			}
+		}
+		al_flip_display();
+	}
+
+	
+
+
+	//al_flip_display();
 	al_destroy_font(font);
+}
+
+bool HowToPlay::getHowDone(){
+	return howDone;
+}
+
+void HowToPlay::setHowDone(bool doneValue){
+	howDone = doneValue;
+}
+
+bool HowToPlay::getActive(){
+	return active;
+}
+
+void HowToPlay::setActive(bool activeValue){
+	active = activeValue;
 }
