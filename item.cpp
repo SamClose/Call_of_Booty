4752953706f2708bec599a060a1cab6a20f22081
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-26 22:50:13 -0800

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,34 +1,31 @@
-#ifndef ITEM_H
-#define ITEM_H
-
-#include <string>
+#include "item.h"
 using namespace std;
 
-class Item {
-private:
-	// need to change this to const char * instead for allegro al_draw_text()
-	string itemName;
-	string itemType;
-	int attackBonus, healthBonus, speedBonus;
+Item::Item() {}
 
-public:
-	Item();
-	Item(string name, string type);
+Item::Item(string name, string type) {
+	itemName = name;
+	itemType = type;
+}
 
-	// need to change this to const char * instead for allegro al_draw_text()
-	string getItemName();
-	void setItemName(string);
+// need to change this to const char * instead for allegro al_draw_text()
+string Item::getItemName() { return itemName; }
+// need to change string name to const char * name instead for allegro al_draw_text()
+void Item::setItemName(string name) { itemName = name; }
 
-	string getItemType();
-	void setItemType(string);
+string Item::getItemType() { return itemType; }
 
-	int getAttackBonus();
-	void setAttackBonus(int);
-	
-	int getHealthBonus();
-	void setHealthBonus(int);
+void Item::setItemType(string type) { itemType = type; }
 
-	int getSpeedBonus();
-	void setSpeedBonus(int);
-};
-#endif
+int Item::getAttackBonus() { return attackBonus; }
+
+void Item::setAttackBonus(int bonus) { attackBonus = bonus; }
+
+int Item::getHealthBonus() { return healthBonus; }
+
+void Item::setHealthBonus(int bonus) { healthBonus = bonus; }
+
+int Item::getSpeedBonus() { return speedBonus; }
+
+void Item::setSpeedBonus(int bonus) { speedBonus = bonus; }
+/* End of item.cpp */
