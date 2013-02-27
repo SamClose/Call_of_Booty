15eb48692b36c0d1c91d2225062c3f76880a0e74
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-26 22:51:05 -0800

    Update item.h

--- item.h
+++ item.h
@@ -1,6 +1,9 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <string>
+using namespace std;
+
 // Create subclasses i.e.
 //		food: fruit, water, meat, bread, cheese
 //		weapons: cannonballs, scattershot, chainballs, explosive cannonballs
@@ -8,27 +11,32 @@
 //	Modifications: can make formulas to create certain items like mast, sails, chassy
 //			this is the simplest form to finish game
 //		repair: tools, wood (1 tool + 1 wood = % shipHealth)
+
 class Item {
-	private:
-		string itemName;
-		string itemType;
-		int attackBonus, healthBonus, speedBonus;
+private:
+	// need to change this to const char * instead for allegro al_draw_text()
+	string itemName;
+	string itemType;
+	int attackBonus, healthBonus, speedBonus;
 
-	public:	
-		string getItemName();
-		void setItemName(string name);
+public:
+	Item();
+	Item(string name, string type);
 
-		string getItemType();
-		void setItemType(string name);
-		
-		int getAttackBonus();
-		void setAttackBonus(int bonus);
-		
-		int getHealthBonus();
-		void setHealthBonus(int bonus);
-		
-		int getSpeedBonus();
-		void setSpeedBonus(int bonus);
-}
+	// need to change this to const char * instead for allegro al_draw_text()
+	string getItemName();
+	void setItemName(string);
 
+	string getItemType();
+	void setItemType(string);
+
+	int getAttackBonus();
+	void setAttackBonus(int);
+	
+	int getHealthBonus();
+	void setHealthBonus(int);
+
+	int getSpeedBonus();
+	void setSpeedBonus(int);
+};
 #endif
