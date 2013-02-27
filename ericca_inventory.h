b2a9482bd3ac447663b4de693be181cf6b25b04a
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-27 08:54:49 -0800

    Update ericca_inventory.h

--- ericca_inventory.h
+++ ericca_inventory.h
@@ -1,42 +1,37 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
-#include "item.cpp"
+#include "item.h"
+using namespace std;
 
 class Inventory {
+private:
+	const int MAX_SPACE = 100;
+	int filledSpace, emptySpace, foodSpace, weaponSpace, repairSpace;
+	Item inventorySpace[MAX_SPACE];
 
-  	private:
-		// want to be able to display so user can see how much space is left
-		int filledSpace, emptySpace, foodSpace, weaponSpace, repairSpace;
+public:
+	// initializes values
+	Inventory();
+	~Inventory();
 
-		// using Item class to help create an array
-		Item inventorySpace[MAX_SPACE];
+	// adds or removes items from Item[]
+	void addItem(Item);
+	int useItem(string);
 	
-	public:
-		// sets filledSpace to 0
-    		Inventory();
-		~Inventory();
+	int getFilledSpace();
+	void setFilledSpace(int);
 
-		// changes space values
-		void addItem(Item item);
-		
-		// If return 0 print out on game, "Item doesn't exist in inventory."
-		int useItem(string itemType);
-		
-		int getFilledSpace();
-		void setFilledSpace(int space);
-		
-		int getEmptySpace();
-		void setEmptySpace(int space);
-		
-		int getFoodSpace();
-		void setFoodSpace(int space);
-		
-		int getWeaponSpace();
-		void getWeaponSpace(int space);
-		
-		int getRepairSpace();
-		void setRepairSpace(int space);
+	int getEmptySpace();
+	void setEmptySpace(int);
+
+	int getFoodSpace();
+	void setFoodSpace(int);
+
+	int getWeaponSpace();
+	void setWeaponSpace(int);
+
+	int getRepairSpace();
+	void setRepairSpace(int);
 };
-
 #endif
