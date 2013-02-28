365f254d21cbcbd37b56fa8c823b56c41192aa0a
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-28 10:26:49 -0800

    Update boss.cpp

--- boss.cpp
+++ boss.cpp
@@ -1,32 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
+
 #include "boss.h"
 using namespace std;
 
-#define MAX_HEALTH 1000
-#define MAX_ATTACK 100
-#define MIN_ATTACK 30
-#define MAX_DEFEND 20
-#define MIN_DEFEND 0
-#define MAX_SPEED 20
-#define MIN_SPEED 5
-
+// Constructor
 Boss::Boss() {
 	health = MAX_HEALTH;
 	attack = MAX_ATTACK;
 }
 
-int Boss::bossAttack() {
-	// ensures that it will truly be a random value not the same
+// Basic attack
+int Boss::bite() {
 	srand(time(NULL));
-	// returns a number between MIN_ATTACK and attack
 	return MIN_ATTACK + rand()%(attack);
 }
 
-/* This attack slows down main character. */
-int Boss::bossSlime() {
+// Slows down enemy
+int Boss::slime() {
 	srand(time(NULL));
-	// returns a number between MIN_SPEED and MAX_SPEED
 	return MIN_SPEED + rand()%(MAX_SPEED);
 }
 
@@ -36,43 +28,29 @@ int Boss::fireBlast() {
 }
 
 int Boss::waterBlast() {
-	srand(time(NULL)):
+	srand(time(NULL));
 	return MIN_ATTACK + rand()%(attack/3);
 }
 
-int Boss::defend() {
+void Boss::defend(int attack) {
 	srand(time(NULL));
-	return MIN_DEF + rand()%(MAX_DEF);
+	int def = MIN_DEFENSE + rand()%(MAX_DEFENSE);
+	setHealth(def - attack);
 }
 
 void Boss::tidalWave() {
-	if(health != MAX_HEALTH) {
-		setBossHealth(health + 10);
-	}
+	if(health != MAX_HEALTH) 
+		setHealth(health + 10);
 }
 
+const char *Boss::getBossName() { return bossName; }
 
-string getBossName() {
-	return bossName;
-}
+int Boss::getHealth() { return health; }
 
-void setBossName(string name) {
-	bossName = name;
-}
+int Boss::getAttack() { return attack; }
 
-int getHealth() {
-	return health;
-}
+void Boss::setBossName(const char *name) { bossName = name; }
 
-void setHealth(int value) {
-	health = value;
-}
+void Boss::setHealth(int num) { health = num; }
 
-int getAttack() {
-	return attack;
-}
-
-void setAttack(int value) {
-	attack = value;
-}
-/* End of boss.cpp */
+void Boss::setAttack(int num) { attack = num; }
