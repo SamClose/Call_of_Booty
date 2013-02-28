75275209eedc6ee0b5f0b651c46cb0ff342892be
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-02-28 10:27:59 -0800

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 Item::Item() {}
 
-Item::Item(string name, string type) {
+Item::Item(const char *name, string type) {
 	itemName = name;
 	itemType = type;
 }
 
+Item::~Item() {}
+
 // need to change this to const char * instead for allegro al_draw_text()
-string Item::getItemName() { return itemName; }
+const char *Item::getItemName() { return itemName; }
 // need to change string name to const char * name instead for allegro al_draw_text()
-void Item::setItemName(string name) { itemName = name; }
+void Item::setItemName(const char *name) { itemName = name; }
 
 string Item::getItemType() { return itemType; }
 
@@ -28,4 +30,3 @@ void Item::setHealthBonus(int bonus) { healthBonus = bonus; }
 int Item::getSpeedBonus() { return speedBonus; }
 
 void Item::setSpeedBonus(int bonus) { speedBonus = bonus; }
-/* End of item.cpp */
