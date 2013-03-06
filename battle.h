7704e8f7a53bf0003c51ffda61973a318cfcf48d
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-03-06 14:24:04 -0800

    Update battle.h

--- battle.h
+++ battle.h
@@ -9,38 +9,48 @@ private:
 	// default display size
 	static const int SCREEN_WIDTH = 800;
 	static const int SCREEN_HEIGHT = 600;
+	static const int FPS = 60;
 
 	// Ship's side view sprite
 	ALLEGRO_BITMAP *player;
 	ALLEGRO_BITMAP *battleBG;
-	ALLEGRO_FONT *font;
 
 	ALLEGRO_COLOR black;
 	ALLEGRO_COLOR white;
 	ALLEGRO_COLOR red;
 	ALLEGRO_COLOR lime;
 
+	ALLEGRO_EVENT_QUEUE *eventQueue;
+
+	ALLEGRO_FONT *font;
+
+	ALLEGRO_TIMER *timer;
+
 	// pixels of HP bars they're technically the same length
 	int userHP, enemyHP;
 
-	// bool to keep track of attacks
-	// if userTurn let user pick attack
-	// else deactivate iterators and let enemy attack
-	bool userTurn;
+	const char *enemyName, *userName;
 
-	// draws battle screen for Ship and an Enemy
-	void draw(Ship, const char *name);
+	// draws battle screen
+	void draw();
+	// updates HP bars
+	void update();
 
 public:
 	// sets private attributes
 	Battle();
+	// destroys timer and eventQueue
+	~Battle();
 
-	// randomly creates enemy
+	// creates an enemy
 	// takes in a premade Ship class
 	// user gets to attack first
 	// switch if it's user turn or enemy's turn
 	// use randomizer to select attacks
-	void shipBattle(Ship); 
-	void bossBattle(Ship);
+	bool shipBattle(Ship*); 
+	bool bossBattle(Ship*);
+
+	// If return 1 -> user won go to Map
+	// If return 0 -> user lost go to GameOver	
 };
 #endif
