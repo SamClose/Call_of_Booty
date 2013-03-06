f6455886a68704123b4672ccea7886ff4a7c46a0
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-03-06 15:23:06 -0800

    Update ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -9,26 +9,29 @@ using namespace std;
 Ship::Ship() : health(MAX_HEALTH), attack(MAX_ATTACK), speed(MAX_SPEED) {}
 
 // deallocates array memory
-Ship::~Ship() { inventory.~Inventory(); }
+Ship::~Ship() { inventory->~Inventory(); }
 
-void Ship::addItem(Item item) { inventory.addItem(item); }
+Inventory *Ship::getInventory() { return inventory; }
+
+void Ship::addItem(Item item) { inventory->addItem(item); }
 
 void Ship::useItem(Item item) {
-	int newCrewHealth, newShipHealth, newAttack;
+	int newCrewHealth = 0, newShipHealth = 0, newAttack = 0;
 	if(item.getItemType() == "food") {
 		if (newCrewHealth < MAX_HEALTH/2) {
-			newCrewHealth = crew.getHealth() + inventory.useItem(item.getItemType());
+			newCrewHealth = crew.getHealth() + inventory->useItem(item);
 			crew.setHealth(newCrewHealth);
 		}
 	}
 	if(item.getItemType() == "repair") {
 		if (newShipHealth < MAX_HEALTH) {
-			newShipHealth = getHealth() + inventory.useItem(item.getItemType());
+			newShipHealth = getHealth() + inventory->useItem(item);
 			setHealth(newShipHealth);
 		}
 	}
 	if(item.getItemType() == "weapon") {
-		setAttack(MAX_ATTACK + inventory.useItem(item.getItemType)/MAX_ATTACK);
+		newAttack = MAX_ATTACK + inventory->useItem(item);
+		setAttack(newAttack);
 	}
 }
 
@@ -37,12 +40,11 @@ void Ship::setCrew() {
 }
 
 void Ship::setInventory() {
-	inventory = Inventory();	
+	inventory = new Inventory();	
 }
 // gives the most damage to enemy
 int Ship::explosiveCannonball() { 
 	// picks a number between MAX_ATTACK and half of ship's health
-	srand(time(NULL));
 	int n = attack + rand()%(health/2);
 	return n;
 }
@@ -50,7 +52,6 @@ int Ship::explosiveCannonball() {
 // designed to give extensive damage to enemy
 int Ship::cannonball() {
 	// randomly picks a number between 10 and the attack
-	srand(time(NULL));
 	int n = 10 + rand()%attack;
 	return n;
 }
@@ -58,14 +59,12 @@ int Ship::cannonball() {
 // reduces enemy's speed
 int Ship::chainShot() {
 	// randomly picks a number between 1 and the speed
-	srand(time(NULL));
 	int n = 1 + rand()%speed;
 	return n;
 }
 
 // 60% attack 30% speed reduction
 int Ship::scatterShot() {
-	srand(time(NULL));
 	// probably be around 5 to 10
 	int a = 5 + rand()%(attack/2);
 	// be around 1 to 7
@@ -90,3 +89,4 @@ void Ship::setHealth(int num) { health = num; }
 void Ship::setAttack(int num) { attack = num; }
 
 void Ship::setSpeed(int num) { speed = num; }
+/* End of ship.cpp */
