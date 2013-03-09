94ebf67e4769367ab66465aa1a1f4028ef200b5e
ericcacalaycay <ericcacalaycay@yahoo.com>
2013-03-09 14:22:36 -0800

    Update enemyShip.cpp

--- enemyShip.cpp
+++ enemyShip.cpp
@@ -2,17 +2,24 @@
 #include "enemyShip.h"
 using namespace std;
 
-EnemyShip::~EnemyShip() { inventory.~Inventory(); }
+EnemyShip::~EnemyShip() { inventory->~Inventory(); }
 
 void EnemyShip::setShipInventory() {
-	inventory.setEmptySpace(MAX_SPACE);
+	inventory->setEmptySpace(MAX_SPACE);
+	for(int i = 0; i < 17; i++) {
+		addItem(inventory->getCannonballItem());
+		addItem(inventory->getChainShotItem());
+		addItem(inventory->getScatterShotItem());
+	}
+	for(int i = 0; i < 3; i++) {
+		addItem(inventory->getWood());
+		addItem(inventory->getTools());
+		addItem(inventory->getRope());
+	}
 }
 
 void EnemyShip::addItem(Item item) {
-	if(inventory.getFilledSpace() < inventory.getEmptySpace() && inventory.getFilledSpace() >= 0)
+	// hypothetically fills spaces 60
+	if(inventory->getFilledSpace() < inventory->getEmptySpace() && inventory->getFilledSpace() >= 0)
 		Ship::addItem(item);
-}
-
-void EnemyShip::useItem(Item item) {
-	Ship::useItem(item);
 }
